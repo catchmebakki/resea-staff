42725b3a8635a1b263dcb217ec8227ce813f3224
catchmebakki <catchmebakki@gmail.com>
2025-05-10 07:24:44 +0530

    Add files via upload

---
+++ CTY.sql
@@ -0,0 +1,380 @@
+SET DEFINE OFF;
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (12, 1999, 'NA', 'Out-of-State (AL)', 'AL', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (13, 2999, 'NA', 'Out-of-State (AK)', 'AK', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (14, 4999, 'NA', 'Out-of-State (AZ)', 'AZ', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (15, 5999, 'NA', 'Out-of-State (AR)', 'AR', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (16, 6999, 'NA', 'Out-of-State (CA)', 'CA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (17, 8999, 'NA', 'Out-of-State (CO)', 'CO', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (18, 9999, 'NA', 'Out-of-State (CT)', 'CT', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (19, 10999, 'NA', 'Out-of-State (DE)', 'DE', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (20, 11999, 'NA', 'Out-of-State (DC)', 'DC', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (21, 12999, 'NA', 'Out-of-State (FL)', 'FL', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (22, 13999, 'NA', 'Out-of-State (GA)', 'GA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (23, 15999, 'NA', 'Out-of-State (HI)', 'HI', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (24, 16999, 'NA', 'Out-of-State (ID)', 'ID', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (25, 17999, 'NA', 'Out-of-State (IL)', 'IL', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (26, 18999, 'NA', 'Out-of-State (IN)', 'IN', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (27, 19999, 'NA', 'Out-of-State (IA)', 'IA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (28, 20999, 'NA', 'Out-of-State (KS)', 'KS', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (29, 21999, 'NA', 'Out-of-State (KY)', 'KY', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (30, 22999, 'NA', 'Out-of-State (LA)', 'LA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (31, 23999, 'NA', 'Out-of-State (ME)', 'ME', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (32, 24999, 'NA', 'Out-of-State (MD)', 'MD', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (33, 25999, 'NA', 'Out-of-State (MA)', 'MA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (34, 26999, 'NA', 'Out-of-State (MI)', 'MI', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (35, 27999, 'NA', 'Out-of-State (MN)', 'MN', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (36, 28999, 'NA', 'Out-of-State (MS)', 'MS', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (37, 29999, 'NA', 'Out-of-State (MO)', 'MO', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (38, 30999, 'NA', 'Out-of-State (MT)', 'MT', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (39, 31999, 'NA', 'Out-of-State (NE)', 'NE', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (40, 32999, 'NA', 'Out-of-State (NV)', 'NV', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (41, 34999, 'NA', 'Out-of-State (NJ)', 'NJ', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (42, 35999, 'NA', 'Out-of-State (NM)', 'NM', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (43, 36999, 'NA', 'Out-of-State (NY)', 'NY', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (44, 37999, 'NA', 'Out-of-State (NC)', 'NC', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (45, 38999, 'NA', 'Out-of-State (ND)', 'ND', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (46, 39999, 'NA', 'Out-of-State (OH)', 'OH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (47, 40999, 'NA', 'Out-of-State (OK)', 'OK', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (48, 41999, 'NA', 'Out-of-State (OR)', 'OR', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (49, 42999, 'NA', 'Out-of-State (PA)', 'PA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (50, 44999, 'NA', 'Out-of-State (RI)', 'RI', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (51, 45999, 'NA', 'Out-of-State (SC)', 'SC', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (52, 46999, 'NA', 'Out-of-State (SD)', 'SD', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (53, 47999, 'NA', 'Out-of-State (TN)', 'TN', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (54, 48999, 'NA', 'Out-of-State (TX)', 'TX', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (55, 49999, 'NA', 'Out-of-State (UT)', 'UT', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (56, 50999, 'NA', 'Out-of-State (VT)', 'VT', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (57, 51999, 'NA', 'Out-of-State (VA)', 'VA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (58, 53999, 'NA', 'Out-of-State (WA)', 'WA', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (59, 54999, 'NA', 'Out-of-State (WV)', 'WV', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (60, 55999, 'NA', 'Out-of-State (WI)', 'WI', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (61, 56999, 'NA', 'Out-of-State (WY)', 'WY', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (1, 33001, 'NA', 'Belknap', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (2, 33003, 'NA', 'Carroll', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (3, 33005, 'NA', 'Cheshire', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (4, 33007, 'NA', 'Coos', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (5, 33009, 'NA', 'Grafton', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (6, 33011, 'NA', 'Hillsborough', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (7, 33013, 'NA', 'Merrimack', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (8, 33015, 'NA', 'Rockingham', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (9, 33017, 'NA', 'Strafford', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (63, 33999, 'NA', 'NHothers', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (11, 33000, 'NA', 'Default', 'NH', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (62, 99999, 'NA', 'Not-in-USA', 'XX', 
+    NULL);
+Insert into COUNTY_CODES_CTY
+   (CTY_ID, CTY_FIPS_CD, CTY_CITY, CTY_NAME, FK_STA_CD, 
+    FK_WFC_ID)
+ Values
+   (10, 33019, 'NA', 'Sullivan', 'NH', 
+    NULL);
+COMMIT;
